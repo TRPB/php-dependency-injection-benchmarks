75795d4143b213a0c06ff825b3f5f17720fd6587
Tom <tom@r.je>
2015-08-22 17:18:47 +0100

    #20 fixed pimple implementation

--- pimple/test3.php
+++ pimple/test3.php
@@ -57,56 +57,9 @@ unset($j);
 $t1 = microtime(true);
 
 for ($i = 0; $i < 10000; $i++) {
-	$container = new \Pimple\Container();
-
-
-$container['a'] = $container->factory(function ($container) {
-	return new A();
-});
-
-
-$container['b'] = $container->factory(function ($container) {
-	return new B($container['a']);
-});
-	
-	
-$container['c'] = $container->factory(function ($container) {
-	return new C($container['b']);
-});
-
-
-$container['d'] = $container->factory(function ($container) {
-	return new D($container['c']);
-});
-	
-
-$container['e'] = $container->factory(function ($container) {
-	return new E($container['d']);
-});
-
-$container['f'] = $container->factory(function ($container) {
-	return new F($container['e']);
-});
-
-$container['g'] = $container->factory(function ($container) {
-	return new G($container['f']);
-});
-		
-$container['h'] = $container->factory(function ($container) {
-	return new H($container['g']);
-});
-			
-$container['i'] = $container->factory(function ($container) {
-	return new I($container['h']);
-});
-				
-$container['j'] = $container->factory(function ($container) {
-	return new J($container['i']);
-});
-
 	$j = $container['j'];
-	
 }
+
 
 $t2 = microtime(true);
 
